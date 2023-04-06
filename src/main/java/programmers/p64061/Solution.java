5ca51e9aeec903fc9f386e08c05bf9820ec619c9
firbyang <why374@firstbrain.co.kr>
2023-04-06 13:42:10 +0900

    프로그래머스 크레인 인형뽑기 게임(64061)

---
+++ src/main/java/programmers/p64061/Solution.java
@@ -0,0 +1,43 @@
+package programmers.p64061;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+
+    public int solution(int[][] board, int[] moves) {
+        int result = 0;
+        List<LinkedList<Integer>> boardList = new ArrayList<>();
+
+        //2차원 배열 List로 변경
+        for (int line = 0; line < board.length; line++) {
+            LinkedList<Integer> itemList = new LinkedList<>();
+            for (int item = 0; item < board.length; item++) {
+                int itemcode = board[item][line];
+                if (itemcode > 0) {
+                    itemList.add(itemcode);
+                }
+            }
+            boardList.add(itemList);
+        }
+
+        LinkedList<Integer> pickupList = new LinkedList<>();
+
+        //moves에 맞춰서 리스트에서 값 가져오기
+        for (int move : moves) {
+            int index = move - 1;
+            if (!boardList.get(index).isEmpty()) {
+                int pickup = boardList.get(index).pop();
+                if (!pickupList.isEmpty() && pickupList.getLast().equals(pickup)) {
+                    result += 2;
+                    pickupList.pollLast();
+                } else {
+                    pickupList.add(pickup);
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/programmers/p64061/SolutionTest.java
@@ -0,0 +1,30 @@
+package programmers.p64061;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class SolutionTest {
+
+    private int[][] board;
+
+    private int[] moves;
+
+    private final Solution solution;
+
+    SolutionTest() {
+        this.solution = new Solution();
+    }
+
+    @Test
+    void solution() {
+        board = new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+        moves = new int[]{1, 5, 3, 5, 1, 2, 1, 4};
+
+        int result = solution.solution(board, moves);
+        log.info("result = {}", result);
+        assertThat(result).isEqualTo(4);
+    }
+}
