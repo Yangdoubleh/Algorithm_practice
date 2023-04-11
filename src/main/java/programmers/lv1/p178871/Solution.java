bc7e6f863d8b7ac83265c157c5f780023fcd0d24
firbyang <why374@firstbrain.co.kr>
2023-04-11 09:52:22 +0900

    프로그래머스 경마 timeout 수정

--- src/main/java/programmers/lv1/p178871/Solution.java
+++ src/main/java/programmers/lv1/p178871/Solution.java
@@ -1,13 +1,11 @@
 package programmers.lv1.p178871;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
 
     public String[] solution(String[] players, String[] callings) {
         HashMap<String, Integer> playerMap = new HashMap<>();
-        ArrayList<String> result = new ArrayList<>(Arrays.stream(players).collect(Collectors.toList()));
 
         for (int index = 0; index < players.length; index++) {
             playerMap.put(players[index], index);
@@ -18,13 +16,13 @@ class Solution {
 
             playerMap.put(calling, idx - 1);
 
-            String item = result.get(idx - 1);
+            String item = players[idx - 1];
             playerMap.put(item, idx);
 
-            result.remove(idx);
-            result.add(idx - 1, calling);
+            players[idx-1] = calling;
+            players[idx] = item;
         }
 
-        return result.stream().toArray(String[]::new);
+        return players;
     }
 }
